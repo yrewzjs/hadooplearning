38ad7cf2c5fd764c31fe3644aec9b63c20c12cc8
yrewzjs <zhangjstj@163.com>
2019-10-20 20:44:27 +0800

    init repo

---
+++ mrlearning/src/main/java/com/learning/zjs/wordcount/WordcountDriver.java
@@ -0,0 +1,55 @@
+package com.learning.zjs.wordcount;
+
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+/**
+ * 相当于一个yarn集群的客户端，需要在此封装我们的mr程序相关运行参数，指定jar包,最后提交给yarn
+ *  执行：hadoop jar hadooptest-1.0-SNAPSHOT.jar wordcount.WordcountDriver /test/ /test/output/
+ * @author ：yrewzjs
+ * @date ：Created in 2019/10/13 15:48
+ * @modified By：
+ */
+public class WordcountDriver
+{
+    public static void main(String[] args) throws Exception
+    {
+        // 1 获取配置信息，或者job对象实例
+        Configuration configuration = new Configuration();
+        // 8 配置提交到yarn上运行,windows和Linux变量不一致
+//        configuration.set("mapreduce.framework.name", "yarn");
+//        configuration.set("yarn.resourcemanager.hostname", "node22");
+        Job job = Job.getInstance(configuration);
+
+        // 6 指定本程序的jar包所在的本地路径
+//        job.setJar("/home/admin/wc.jar");
+        job.setJarByClass(WordcountDriver.class);
+
+        // 2 指定本业务job要使用的mapper/Reducer业务类
+        job.setMapperClass(WordcountMapper.class);
+        job.setReducerClass(WordcountReducer.class);
+
+        // 3 指定mapper输出数据的kv类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        // 4 指定最终输出的数据的kv类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        // 5 指定job的输入原始文件所在目录
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        // 7 将job中配置的相关参数，以及job所用的java类所在的jar包， 提交给yarn去运行
+//        job.submit();
+        boolean result = job.waitForCompletion(true);
+        System.exit(result ? 0 : 1);
+    }
+}
---
+++ src/main/java/com/learning/zjs/hadoop/TestDataGenerator.java
@@ -0,0 +1,139 @@
+package com.learning.zjs.hadoop;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import cn.binarywang.tools.generator.ChineseAddressGenerator;
+import cn.binarywang.tools.generator.ChineseIDCardNumberGenerator;
+import cn.binarywang.tools.generator.ChineseMobileNumberGenerator;
+import cn.binarywang.tools.generator.ChineseNameGenerator;
+import cn.binarywang.tools.generator.EmailAddressGenerator;
+import cn.binarywang.tools.generator.EnglishNameGenerator;
+import java.util.Calendar;
+import org.apache.commons.lang3.RandomUtils;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author ：yrewzjs
+ * @date ：Created in 2019/10/20 15:35
+ * @modified By：
+ */
+public class TestDataGenerator
+{
+
+    protected File file = null;
+
+    protected FileWriter fw = null;
+
+    public static void main(String[] args)
+    {
+//        String path = args[0];
+        String path = "G:\\CS_Data\\Data_Java\\IdeaProjects\\hadooplearning\\target\\hadooplearningcli\\bigdata_personalinfo.txt";
+//        int rows = Integer.parseInt(args[1]);
+        int rows = 1000000;
+
+        TestDataGenerator generator = new TestDataGenerator();
+        try
+        {
+            // pre
+            generator.preWriteData(path);
+            // do write
+            generator.doWriteData(rows);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            generator.afterWriteData();
+        }
+    }
+
+    public void preWriteData(String path) throws IOException
+    {
+        file = new File(path);
+
+        if (!file.exists())
+        {
+            file.createNewFile();
+        }
+        else
+        {
+            file.delete();
+            file.createNewFile();
+        }
+    }
+
+    public void doWriteData(int rows) throws IOException
+    {
+        System.out.println("start write data...");
+        fw = new FileWriter(file);
+        for (int i = 0; i < rows; i++)
+        {
+            fw.write(getLineContext()); //向文件中写内容
+            fw.write("\r\n");
+            fw.flush();
+        }
+        System.out.println("write data successfully!");
+    }
+
+    public void afterWriteData()
+    {
+        if (fw != null)
+        {
+            try
+            {
+                fw.close();
+            }
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     *  generate a person info
+     *
+     * @return
+     */
+    public String getLineContext()
+    {
+        StringBuilder sb = new StringBuilder();
+        //中文姓名
+        ChineseNameGenerator cng = ChineseNameGenerator.getInstance();
+        sb.append(cng.generate());
+        sb.append(" ");
+        //身份证号码
+        ChineseIDCardNumberGenerator cidcng = (ChineseIDCardNumberGenerator) ChineseIDCardNumberGenerator.getInstance();
+        String cid = cidcng.generate();
+        sb.append(cid);
+        sb.append(" ");
+        // 年龄
+        // StringUtils.leftPad("" + RandomUtils.nextInt(0, 100000000), 8, "0");
+        Calendar date = Calendar.getInstance();
+        String year = String.valueOf(date.get(Calendar.YEAR));
+        int age = Integer.parseInt(year) - Integer.parseInt(cid.substring(6,10));
+        sb.append(age);
+        sb.append(" ");
+        //手机号
+        ChineseMobileNumberGenerator cmng = ChineseMobileNumberGenerator.getInstance();
+        sb.append(cmng.generate());
+        sb.append(" ");
+        //电子邮箱
+        EmailAddressGenerator eag = (EmailAddressGenerator) EmailAddressGenerator.getInstance();
+        sb.append(eag.generate());
+        sb.append(" ");
+        // 薪水
+        sb.append(RandomUtils.nextInt(1000, 100000));
+        sb.append(" ");
+        //居住地址
+        ChineseAddressGenerator cag = (ChineseAddressGenerator) ChineseAddressGenerator.getInstance();
+        sb.append(cag.generate());
+
+        return sb.toString();
+    }
+}
